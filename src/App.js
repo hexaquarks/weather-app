033a1b5f91d4efa641a0a194e11e472229eb833f
physicskush <seseriusca@gmail.com>
2021-08-05 18:58:49 -0400

    Started implementing a burger-menu functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import Page from './components/Page/Page';
+import SideMenu from './components/sideMenu/SideMenu.js';
+import styles from './components/sideMenu/SideMenu.module.css';
+
 
 function App() {
   return (
-    <div className="app">
-      <Page />
+    <div className="app" id="app">
+      <SideMenu pageWrapId={ "page-wrap" } outerContainerId={ "app" }/>
+
+      <div id="page-wrap">
+        <Page />
+      </div>
     </div>
   );
 }
